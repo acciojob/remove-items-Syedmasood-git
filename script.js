397637d02e730c09e6e34cb1723c9709ced4461a
SyedMasood46 <masoodhussaini46@gmail.com>
2023-08-21 23:53:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.getElementById("removeButton").addEventListener("click", function() {
+     let colorSelect = document.getElementById("colorSelect");
+      colorSelect.remove(colorSelect.selectedIndex);
+});
